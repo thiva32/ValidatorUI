82d9b92784cfa2f6426776d3641f235086c9ba11
thiva32 <thiva37@Gmail.com>
2025-03-09 22:09:19 +0800

    store presets in their own folder

--- loadconfig.py
+++ loadconfig.py
@@ -2,7 +2,13 @@ import json
 import os
 
 def load_config(config_preset):
-    config_path = os.path.join(os.path.dirname(__file__), f'{config_preset}.json')
+
+    presets_folder = os.path.join(os.path.dirname(__file__), "presets")
+    config_path = os.path.join(presets_folder, f"{config_preset}.json")
+
+    if not os.path.exists(config_path):
+        print(f'Config file for presets not found: {config_path}')
+        return {}
     with open(config_path, 'r') as f:
         config = json.load(f)
     return config
